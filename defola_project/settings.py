0033ecc205bf00c2f0e1e1e83ce989b760bc2c81
dannykay62 <danielkay62@gmail.com>
2026-07-14 18:09:06 +0100

    fix: switch to native cloudinary storage engine to fix whitenoise collision

--- defola_project/settings.py
+++ defola_project/settings.py
@@ -203,6 +203,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Points to a folder named "static" in your root directory
+]
 
 # Disables Cloudinary's custom collection logic so WhiteNoise can compile flawlessly
 DATABASE_STORAGE_IGNORE_STATICFILES = True
